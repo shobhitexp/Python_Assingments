8f2079093e653777dd8997d608ed27f3887a5fe8
SHOBHIT RAJ <115642349+shobhitexp@users.noreply.github.com>
2023-01-18 09:11:13 +0530

    Generatemail_python
    1 Go to your Google Account.
    2 Select Security.
    3 Under "Signing in to Google," select App Passwords. You may need to sign in. If you don’t have this option, it might be because:
        2-Step Verification is not set up for your account.
        2-Step Verification is only set up for security keys.
    4 Your account is through work, school, or other organization.
    5 You turned on Advanced Protection.
    6 At the bottom, choose Select app and choose the app you using and then Select device and choose the device you’re using and then Generate.
    7 Follow the instructions to enter the App Password. The App Password is the 16-character code in the yellow bar on your device.
        Tap Done.

---
+++ generatemail_python.py
@@ -0,0 +1,12 @@
+import smtplib , ssl
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "shobhitraj4757@gmail.com"  # Enter your address
+receiver_email = "shobhitrajpatna@gmail.com"  # Enter receiver address
+password = 'gvuosegxmedtdoow'
+message = 5*"""This is SHOBHIT RAJ message while learning python 19/1/2023\n"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
